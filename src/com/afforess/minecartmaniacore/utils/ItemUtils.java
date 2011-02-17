b2692428c941563945d255d7327d8440113ba681
afforess <afforess@gmail.com>
2011-02-16 19:25:52 -0500

    Support for detecting multiple items in one string.
    Signed-off-by: afforess <afforess@gmail.com>

--- src/com/afforess/minecartmaniacore/utils/ItemUtils.java
+++ src/com/afforess/minecartmaniacore/utils/ItemUtils.java
@@ -1,10 +1,30 @@
 package com.afforess.minecartmaniacore.utils;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 
 public class ItemUtils {
+	
+	/**
+	 * Returns the first item name or id found in the given string, or null if there was no item name or id.
+	 * If the item name is a partial name, it will match the name with the shortest number of letter.
+	 * Ex ("reds" will match "redstone" (the wire item) and not redstone ore.
+	 * @return material found, or null
+	 */
+	public static Material getFirstItemStringToMaterial(String str) {
+		Material items[] = getItemStringToMaterial(str);
+		return items == null ? null : items[0];
+	}
 
-	public static Material itemStringToMaterial(String str) {
+	/**
+	 * Returns the list of material for each item name or id found in the given string, or null if there was no item name or id.
+	 * If the item name is a partial name, it will match the name with the shortest number of letter.
+	 * Ex ("reds" will match "redstone" (the wire item) and not redstone ore.
+	 * @return materials found, or null
+	 */
+	public static Material[] getItemStringToMaterial(String str) {
+		ArrayList<Material> items = new ArrayList<Material>();
 		if (str.indexOf('[') > -1) {
 			str = str.substring(str.indexOf('[') + 1);
 		}
@@ -12,31 +32,38 @@ public class ItemUtils {
 			str = str.substring(0, str.indexOf(']'));
 		}
 		str = str.toLowerCase();
-		//try and parse it as a number first
-		try {
-			String num = StringUtils.getNumber(str);
-			int id = Integer.parseInt(num);
-			Material type = Material.getMaterial(id);
-			if (type != null) {
-				return type;
-			}
-		}
-		catch (Exception exception) {
-			//ignore
-		}
 		
-		Material bestMaterial = null;
-		for (Material e : Material.values()) {
-			if (e != null) {
-				String item = e.toString().toLowerCase();
-				if (item.contains(str)) {
-					//If two items have the same partial string in them (e.g diamond and diamond shovel) the shorter name wins
-					if (bestMaterial == null || e.toString().length() < bestMaterial.toString().length()) {
-						bestMaterial = e;
+		String[] keys = str.split("-| ?: ?");
+		for (int i = 0; i < keys.length; i++) {
+			Material type = null;
+			//Parse the numbers first. Can be separated by "-" or ":"
+			try {
+				String num = StringUtils.getNumber(keys[i]);
+				int id = Integer.parseInt(num);
+				type = Material.getMaterial(id);
+			}
+			catch (Exception exception) {
+				//Now try string names
+				Material bestMaterial = null;
+				for (Material e : Material.values()) {
+					if (e != null) {
+						String item = e.toString().toLowerCase();
+						if (item.contains(keys[i])) {
+							//If two items have the same partial string in them (e.g diamond and diamond shovel) the shorter name wins
+							if (bestMaterial == null || item.length() < bestMaterial.toString().length()) {
+								bestMaterial = e;
+							}
+						}
 					}
 				}
+				type = bestMaterial;
+			}
+			if (type != null) {
+				items.add(type);
 			}
 		}
-		return bestMaterial;
+
+		Material itemList[] = new Material[items.size()];
+		return items.toArray(itemList);
 	}
 }
