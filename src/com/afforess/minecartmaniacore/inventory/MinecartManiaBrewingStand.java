174ada704ec3d546834228077faf6ffecda6eeec
Rob Nelson <nexis@7chan.org>
2011-12-05 02:07:28 -0800

    Add brewing stand support

---
+++ src/com/afforess/minecartmaniacore/inventory/MinecartManiaBrewingStand.java
@@ -0,0 +1,77 @@
+package com.afforess.minecartmaniacore.inventory;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import org.bukkit.block.BrewingStand;
+import org.bukkit.inventory.Inventory;
+
+public class MinecartManiaBrewingStand extends MinecartManiaSingleContainer implements MinecartManiaInventory {
+    
+    private final Location location;
+    private ConcurrentHashMap<String, Object> data = new ConcurrentHashMap<String, Object>();
+    
+    public MinecartManiaBrewingStand(BrewingStand brewingStand) {
+    	super(brewingStand.getInventory());
+        this.location = brewingStand.getBlock().getLocation();
+    }
+    
+    public int getX() {
+        return this.location.getBlockX();
+    }
+    
+    public int getY() {
+        return this.location.getBlockY();
+    }
+    
+    public int getZ() {
+        return this.location.getBlockZ();
+    }
+    
+    public World getWorld() {
+        return this.location.getWorld();
+    }
+    
+    public Location getLocation() {
+        return location;
+    }
+    
+    public BrewingStand getBrewingStand() {
+    	return (BrewingStand) location.getBlock().getState();
+    }
+    /**
+     ** Returns the value from the loaded data
+     ** 
+     * @param the
+     *            string key the data value is associated with
+     **/
+    public Object getDataValue(String key) {
+        if (data.containsKey(key)) {
+            return data.get(key);
+        }
+        return null;
+    }
+    
+    /**
+     ** Creates a new data value if it does not already exists, or resets an existing value
+     ** 
+     * @param the
+     *            string key the data value is associated with
+     ** @param the
+     *            value to store
+     **/
+    public void setDataValue(String key, Object value) {
+        if (value == null) {
+            data.remove(key);
+        } else {
+            data.put(key, value);
+        }
+    }
+    
+    public Inventory getInventory() {
+    	return getBrewingStand().getInventory();
+    }
+}
+
