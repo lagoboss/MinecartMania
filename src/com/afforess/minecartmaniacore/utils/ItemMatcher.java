658bfe6fe75adb6020f521bc4acebca619a26265
N3X15 <nexis@7chan.org>
2011-12-12 14:34:26 -0800

    butts

--- src/com/afforess/minecartmaniacore/utils/ItemMatcher.java
+++ src/com/afforess/minecartmaniacore/utils/ItemMatcher.java
@@ -13,69 +13,73 @@ import com.afforess.minecartmaniacore.matching.MatchToken;
 /**
  * An efficient system for matching items based on criteria provided by signs.
  * 
- * Each ItemMatcher is used to match against a single "line" on a sign.  If the ItemMatcher 
- * cannot see a match against the current item, the item is passed to the next ItemMatcher.
+ * Each ItemMatcher is used to match against a single "line" on a sign. If the ItemMatcher cannot see a match against the current item, the item is passed to the next ItemMatcher.
  * 
- * A line that reads: 373;8192-8197@3
- * Would turn into the following:
- *  * ITEM ID IS EQUAL TO 373
- *  * DATA IS IN RANGE 8192 TO 8197
- * (amount is not compared, only returned via getAmount() to systems that need it) 
+ * A line that reads: 373;8192-8197@3 Would turn into the following: * ITEM ID IS EQUAL TO 373 * DATA IS IN RANGE 8192 TO 8197 (amount is not compared, only returned via getAmount() to systems that need it)
+ * 
  * @author Rob
- *
+ * 
  */
 public class ItemMatcher {
     // List of matching tokens
     protected List<MatchToken> matchTokens = new ArrayList<MatchToken>();
     
     // Number of things requested (-1 = N/A)
-    protected int amount=64;
+    protected int amount = -1;
     
-    public ItemMatcher() {}
-
+    public ItemMatcher() {
+    }
+    
     public ItemMatcher(int typeId, short durability) {
-        addConstant(MatchField.TYPE_ID,typeId);
-        if(durability!=-1) {
-            addConstant(MatchField.DURABILITY,durability);
+        addConstant(MatchField.TYPE_ID, typeId);
+        if (durability != -1) {
+            addConstant(MatchField.DURABILITY, durability);
         }
     }
-    public void addRange(MatchField field,int start, int end) {
-        matchTokens.add(new MatchInRange(field,start,end));
+    
+    public void addRange(MatchField field, int start, int end) {
+        matchTokens.add(new MatchInRange(field, start, end));
     }
-    public void addConstant(MatchField field,int value) {
-        matchTokens.add(new MatchConstant(field,value));
+    
+    public void addConstant(MatchField field, int value) {
+        matchTokens.add(new MatchConstant(field, value));
     }
-
+    
     public void addExpression(MatchToken token) {
         matchTokens.add(token);
     }
     
     /**
      * See if them item fits ALL of the criteria listed in matchTokens.
+     * 
      * @param item
      * @return
      */
     public boolean match(ItemStack item) {
-        if(item==null) return false;
-        for(MatchToken matcher : matchTokens) {
-            if(!matcher.match(item)) return false;
+        if (item == null)
+            return false;
+        for (MatchToken matcher : matchTokens) {
+            if (!matcher.match(item))
+                return false;
         }
         return true;
     }
-
+    
     /**
      * Used in the sign parser to get an itemstack from the (simple) criteria.
+     * 
      * @return
      */
     public ItemStack toItemStack() {
-        ItemStack item = new ItemStack(0,1,(short)-1);
-        boolean foundID=false;
-        for(MatchToken matcher : matchTokens) {
-            if(matcher.isComplex()) return null;
-            if(matcher instanceof MatchConstant) {
-                MatchConstant constant = (MatchConstant)matcher;
+        ItemStack item = new ItemStack(0, 1, (short) -1);
+        boolean foundID = false;
+        for (MatchToken matcher : matchTokens) {
+            if (matcher.isComplex())
+                return null;
+            if (matcher instanceof MatchConstant) {
+                MatchConstant constant = (MatchConstant) matcher;
                 int v = constant.getValue();
-                switch(constant.getField()) {
+                switch (constant.getField()) {
                     case TYPE_ID:
                         item.setTypeId(v);
                         break;
@@ -86,17 +90,18 @@ public class ItemMatcher {
             }
         }
         
-        if(!foundID) return null;
+        if (!foundID)
+            return null;
         
         item.setAmount(amount);
         
         return item;
     }
-
+    
     public void setAmount(int amount) {
-        this.amount=amount;
+        this.amount = amount;
     }
-
+    
     public int getAmount() {
         return amount;
     }
@@ -104,11 +109,16 @@ public class ItemMatcher {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("MATCH (AND):\n{\n");
-        for(MatchToken mt : matchTokens) {
+        for (MatchToken mt : matchTokens) {
             sb.append(mt.toString(1));
             sb.append("\n");
         }
         sb.append("}");
         return sb.toString();
+    }
+    
+    public int getAmountForTransfer() {
+        // TODO Auto-generated method stub
+        return amount == -1 ? 64 : amount;
     }
 }
