dddb7c5302a85c473cd2cd78035dbd121824c722
N3X15 <nexis@7chan.org>
2011-12-23 19:22:16 -0800

    Add some debugging

--- src-test/com/afforess/minecartmaniacore/utils/TestMatching.java
+++ src-test/com/afforess/minecartmaniacore/utils/TestMatching.java
@@ -10,6 +10,7 @@ public class TestMatching extends TestCase {
         // Should match all types of logs.
         ItemMatcher m = new ItemMatcher();
         m.parse("17");
+        System.out.println(m.toString());
         assertTrue("Failed to match 17;0", m.match(new ItemStack(17, 1, (short) 0)));
         assertTrue("Failed to match 17;1", m.match(new ItemStack(17, 1, (short) 1)));
         assertTrue("Failed to match 17;2", m.match(new ItemStack(17, 1, (short) 2)));
@@ -19,6 +20,7 @@ public class TestMatching extends TestCase {
         // Should match only match 17;0.
         ItemMatcher m = new ItemMatcher();
         m.parse("17;0");
+        System.out.println(m.toString());
         assertTrue("Failed to match 17;0", m.match(new ItemStack(17, 1, (short) 0)));
         assertFalse("Failed to NOT match 17;1", m.match(new ItemStack(17, 1, (short) 1)));
         assertFalse("Failed to NOT match 17;2", m.match(new ItemStack(17, 1, (short) 2)));
@@ -28,6 +30,7 @@ public class TestMatching extends TestCase {
         // Should match everything.
         ItemMatcher m = new ItemMatcher();
         m.parse("all items");
+        System.out.println(m.toString());
         assertTrue("Failed to match 17;0", m.match(new ItemStack(17, 1, (short) 0)));
         assertTrue("Failed to match 373;8196", m.match(new ItemStack(373, 1, (short) 8196)));
     }
@@ -36,6 +39,7 @@ public class TestMatching extends TestCase {
         // Should match everything BUT logs.
         ItemMatcher m = new ItemMatcher();
         m.parse("all items:!17;0");
+        System.out.println(m.toString());
         assertFalse("Failed to NOT match 17;0", m.match(new ItemStack(17, 1, (short) 0)));
         assertTrue("Failed to match 373;8196", m.match(new ItemStack(373, 1, (short) 8196)));
     }
